5962e82f7657e6f7d46a5e07dc7edc69ddb65564
Okagi <117276789+Okagi@users.noreply.github.com>
2022-11-23 10:02:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace massive_max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] a = new int[] { 1, 8, 5, 3, 9, 1, 0, 7, 2, 4 };
+            Console.WriteLine();
+            for (int j = 0; j < a.Length; j++)
+                Console.WriteLine(a[j] + "");
+            Console.WriteLine();
+            int max = 0;
+            for (int b = 0; b < a.Length - 1; b++)
+
+            {
+                if (a[b] > max)
+                    max = a[b];
+
+            }
+            Console.WriteLine(max);
+
+
+        }
+    }
+}       
+            
+     
+   
+
